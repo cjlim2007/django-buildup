41fd66a71cafc69d9eb32a7b04c56cc1a3e2f6e6
cjlim2007 <christopherlim@slategrey-nav.local>
2015-02-09 16:57:57 -0800

    added time

--- buildup/urls.py
+++ buildup/urls.py
@@ -1,2 +1,2 @@
 from django.conf.urls import patterns, url
-urlpatterns = patterns('', url(r'^$', 'buildup.views.hello', name='hello'))
+urlpatterns = patterns('', url(r'^$', 'buildup.views.hello', name='hello'), url(r'^time/$', 'buildup.views.time', name='time'))
--- buildup/views.py
+++ buildup/views.py
@@ -1,3 +1,7 @@
+from datetime import datetime
 from django.http import HttpResponse
 def hello(request):
     return HttpResponse("Hello world!")
+
+def time(request):
+    return HttpResponse("The time is " + str(datetime.now()))
