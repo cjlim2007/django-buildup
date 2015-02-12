44d2fb3909f0c67d99980e89759af860d6d4b57a
Hurshal Patel <hpatel516@gmail.com>
2015-02-11 16:56:44 -0800

    updates

--- buildup/urls.py
+++ buildup/urls.py
@@ -1,2 +1,5 @@
 from django.conf.urls import patterns, url
-urlpatterns = patterns('', url(r'^$', 'buildup.views.hello', name='hello'))
+urlpatterns = patterns('',
+    url(r'^$', 'buildup.views.hello', name='hello'),
+    url(r'^hello_template/(?P<yourname>.+)/$', 'buildup.views.hello_template', name='hello_template')
+)
--- buildup/views.py
+++ buildup/views.py
@@ -1,3 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 def hello(request):
     return HttpResponse("Hello world!")
+
+def hello_template(request, yourname):
+    return HttpResponse("Hello {}".format(yourname))
