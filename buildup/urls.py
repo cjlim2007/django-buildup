2ace54832a1af7cf0d9792d9ac9ea44f807253ba
Hurshal Patel <hpatel516@gmail.com>
2015-02-15 09:33:19 -0800

    add templates

--- buildup/urls.py
+++ buildup/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'buildup.views.hello', name='hello'),
-    url(r'^hello_template/(?P<yourname>.+)/$', 'buildup.views.hello_template', name='hello_template')
+    url(r'^time/$', 'buildup.views.time', name='time'),
+    url(r'^random/$', 'buildup.views.random', name='random'),
+    url(r'^hello/(?P<yourname>\w+)/$', 'buildup.views.hello_template', name='hello_template'),
+    url(r'^speak/(?P<sentence>[^/]+)/$', 'buildup.views.speak', name='speak')
 )
--- buildup/views.py
+++ buildup/views.py
@@ -1,7 +1,22 @@
+from datetime import datetime
+from random import randint
+import subprocess
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def hello(request):
     return HttpResponse("Hello world!")
 
+def time(request):
+    return render(request, "time.html", { "current_time": str(datetime.now()) })
+
+def random(request):
+    return render(request, "random.html", { "random_number": randint(0, 10) })
+
 def hello_template(request, yourname):
-    return HttpResponse("Hello {}".format(yourname))
+    return render(request, "hello.html", { "yourname": yourname, "foobar": 12 })
+
+def speak(request, sentence):
+    subprocess.call(["say", sentence])
+    return render(request, "speak.html")
