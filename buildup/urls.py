1240209c8bf5656c4ef683b48131d70af43e54c8
Hurshal Patel <hpatel516@gmail.com>
2015-02-18 08:53:08 -0800

    add new fact page

--- buildup/urls.py
+++ buildup/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^hello/(?P<yourname>\w+)/$', 'buildup.views.hello_template', name='hello_template'),
     url(r'^speak/(?P<sentence>\w+)/$', 'buildup.views.speak', name='speak'),
     url(r'^facts/$', 'buildup.views.all_facts', name='all_facts'),
+    url(r'^facts/new/$', 'buildup.views.new_fact', name='new_fact')
 )
--- buildup/views.py
+++ buildup/views.py
@@ -2,8 +2,10 @@ from datetime import datetime
 from random import randint
 import subprocess
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+from django.core.urlresolvers import reverse
 
 from buildup.models import Fact
 
@@ -25,3 +27,29 @@ def speak(request, sentence):
 
 def all_facts(request):
     return render(request, "all_facts.html", { "facts": Fact.objects.all() })
+
+class FactForm(forms.Form):
+    text = forms.CharField(label="A random fact", max_length=255)
+    author = forms.CharField(label="Your name", max_length=255)
+
+def new_fact(request):
+    # someone wants to create a fact
+    if request.method == "GET":
+        form = FactForm()
+        return render(request, "new_fact.html", { "form": form })
+    else:
+        # someone submitted the form so we need to save the data
+        form = FactForm(request.POST)
+
+        if form.is_valid():
+            # save the new fact
+            text = form.cleaned_data['text']
+            author = form.cleaned_data['author']
+            new_fact = Fact(text=text, author=author, created_date=datetime.now())
+            new_fact.save()
+
+            # and redirect to the all_facts page
+            return HttpResponseRedirect(reverse('all_facts'))
+        else:
+            # return the form with any errors
+            return render(request, "new_fact.html", { "form": form })
