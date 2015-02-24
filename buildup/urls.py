6f411c0a79fb333a03fd6458b81a97be8b8f7f40
cjlim2007 <christopherlim@slategrey-nav.local>
2015-02-23 17:24:56 -0800

    fixed.. probably

--- buildup/urls.py
+++ buildup/urls.py
@@ -1,2 +1,2 @@
 from django.conf.urls import patterns, url
-urlpatterns = patterns('', url(r'^$', 'buildup.views.hello', name='hello'), url(r'^hello_template/(?P<yourname>\w+)/(?P<yoursentence>.+)/$', 'buildup.views.hello_template', name='hello_template'), url(r'^facts/$', 'buildup.views.all_facts', name='facts'))
+urlpatterns = patterns('', url(r'^$', 'buildup.views.hello', name='hello'), url(r'^hello_template/(?P<yourname>\w+)/(?P<yoursentence>.+)/$', 'buildup.views.hello_template', name='hello_template'), url(r'^facts/$', 'buildup.views.all_facts', name='all_facts'))
